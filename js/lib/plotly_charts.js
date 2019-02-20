8d378a98153cf0c2a75e16260ab1e12d03b2d924
Adrian <adr.lar@me.com>
2019-02-20 17:17:43 +0000

    putting platform colours in plotly chart
    Also removing the bad bar chart. Will add in another chart there later.

--- js/lib/plotly_charts.js
+++ js/lib/plotly_charts.js
@@ -1,3 +1,59 @@
+facility_colour_map = {
+	"Tissue Profiling": "#8dd3c7", // Affinity Proteomics
+	"Fluorescence Tissue Profiling": "#8dd3c7", // Affinity Proteomics
+	"Fluorescence Correlation Spectroscopy": "#ffffb3", // Bioimaging
+	"Bioinformatics Compute and Storage": "#bebada", // Bioinformatics
+	"Bioinformatics Long-term Support WABI": "#bebada", // Bioinformatics
+	"Bioinformatics Support and Infrastructure": "#bebada", // Bioinformatics
+	"Systems Biology": "#bebada", // Bioinformatics
+	"Cryo-EM (SU)": "#fb8072", // Cellular and Molecular Imaging
+	"Advanced Light Microscopy (ALM)": "#fb8072", // Cellular and Molecular Imaging
+	"BioImage Informatics": "#fb8072", // Cellular and Molecular Imaging
+	"Cell Profiling": "#fb8072", // Cellular and Molecular Imaging
+	"Cryo-EM": "#fb8072", // Cellular and Molecular Imaging
+	"Cryo-EM (UmU)": "#fb8072", // Cellular and Molecular Imaging
+	"Protein Science Facility (PSF)": "#fb8072", // Cellular and Molecular Imaging
+	"Swedish NMR Centre (SNC)": "#fb8072", // Cellular and Molecular Imaging
+	"Chemical Biology Consortium Sweden (KI)": "#80b1d3", // Chemical Biology and Genome Engineering
+	"Chemical Biology Consortium Sweden (UmU)": "#80b1d3", // Chemical Biology and Genome Engineering
+	"Chemical Biology Consortium Sweden (CBCS)": "#80b1d3", // Chemical Biology and Genome Engineering
+	"Genome Engineering Zebrafish": "#80b1d3", // Chemical Biology and Genome Engineering
+	"High-throughput Genome Engineering (HTGE)": "#80b1d3", // Chemical Biology and Genome Engineering
+	"Clinical Genomics Gothenburg": "#b3de69", // Diagnostics Development
+	"Clinical Genomics Lund": "#b3de69", // Diagnostics Development
+	"Clinical Genomics Stockholm": "#b3de69", // Diagnostics Development
+	"Clinical Genomics Uppsala": "#b3de69", // Diagnostics Development
+	"Drug Discovery and Development (DDD)": "#fdb462", // Drug Discovery and Development
+	"Karolinska High Throughput Center (KHTC)": "#d9d9d9", // Functional Genomics
+	"Ancient DNA": "#fccde5", // Genomics
+	"NGI Stockholm": "#fccde5", // Genomics
+	"NGI Stockholm (Genomics Applications)": "#fccde5", // Genomics
+	"NGI Stockholm (Genomics Production)": "#fccde5", // Genomics
+	"NGI Uppsala (SNP&SEQ Technology Platform)": "#fccde5", // Genomics
+	"NGI Uppsala (Uppsala Genome Center)": "#fccde5", // Genomics
+	"Eukaryotic Single Cell Genomics (ESCG)": "#fccde5", // Genomics
+	"Microbial Single Cell Genomics": "#fccde5", // Genomics
+	"Clinical Biomarkers": "#bc80bd", // Next-Generation Diagnostics
+	"Mass Cytometry (KI)": "#ccebc5", // Proteomics and Metabolomics
+	"Mass Cytometry (LiU)": "#ccebc5", // Proteomics and Metabolomics
+	"Mass Cytometry": "#ccebc5", // Proteomics and Metabolomics
+	"Single Cell Proteomics": "#ccebc5", // Proteomics and Metabolomics
+	"Autoimmunity Profiling": "#ccebc5", // Proteomics and Metabolomics
+	"Chemical Proteomics and Proteogenomics (MBB)": "#ccebc5", // Proteomics and Metabolomics
+	"Chemical Proteomics and Proteogenomics (OncPat)": "#ccebc5", // Proteomics and Metabolomics
+	"Chemical Proteomics & Proteogenomics": "#ccebc5", // Proteomics and Metabolomics
+	"PLA Proteomics": "#ccebc5", // Proteomics and Metabolomics
+	"Plasma Profiling": "#ccebc5", // Proteomics and Metabolomics
+	"Swedish Metabolomics Centre (SMC)": "#ccebc5", // Proteomics and Metabolomics
+	"Clinical Proteomics Mass spectrometry": "#ffed6f", // Regional facilities of national interest
+	"Mass Spectrometry-based Proteomics, Uppsala": "#ffed6f", // Regional facilities of national interest
+	"Array and Analysis Facility": "#ffed6f", // Regional facilities of national interest
+	"Mutation Analysis Facility (MAF)": "#ffed6f", // Regional facilities of national interest
+	"Bioinformatics and Expression Analysis (BEA)": "#ffed6f", // Regional facilities of national interest
+	"BioMaterial Interactions (BioMat)": "#ffed6f", // Regional facilities of national interest
+	"Advanced Mass Spectrometry Proteomics": "#ffed6f" // Regional facilities of national interest
+}
+
 function draw_label_pie(target_div, publications_json, chart_title){
 	// console.log(publications_json);
 	var years = {};
@@ -21,17 +77,36 @@ function draw_label_pie(target_div, publications_json, chart_title){
 			var title = years[year][i]["title"];
 			var labels = years[year][i]["labels"];
 			var pmid = years[year][i]["pmid"];
+
+			// Using these labels because we want to remove some...
+			var used_labels = Object.keys(labels);
+			
+			// Cleaning up the labels with these rules
+			if (used_labels.indexOf("NGI Stockholm (Genomics Applications)") >= 0){
+				used_labels.splice(used_labels.indexOf("NGI Stockholm (Genomics Applications)"), 1);
+				used_labels.splice(used_labels.indexOf("NGI Stockholm (Genomics Production)"), 1);
+				used_labels.push("NGI Stockholm");
+			}
+			if (used_labels.indexOf("NGI Stockholm (Genomics Production)") >= 0){
+				used_labels.splice(used_labels.indexOf("NGI Stockholm (Genomics Applications)"), 1);
+				used_labels.splice(used_labels.indexOf("NGI Stockholm (Genomics Production)"), 1);
+				used_labels.push("NGI Stockholm");
+			}
+			if (used_labels.indexOf("Bioinformatics Compute and Storage") >= 0){
+				used_labels.splice(used_labels.indexOf("Bioinformatics Compute and Storage"), 1);
+			}
+
 			pmids.push(pmid);
 			dois.push(doi);
 			titles_for_word_cloud = titles_for_word_cloud + " " + title;
 			var label_length = 0;
-			for (var key in labels){
+			for (var key in used_labels){
 				//No idea why labels.length doesnt work...
 				label_length += 1;
-				if (label_count.hasOwnProperty(key)){
-					label_count[key] += 1;
+				if (label_count.hasOwnProperty(used_labels[key])){
+					label_count[used_labels[key]] += 1;
 				} else {
-					label_count[key] = 1;
+					label_count[used_labels[key]] = 1;
 				}
 			}
 			if (label_length != 0){
@@ -42,55 +117,56 @@ function draw_label_pie(target_div, publications_json, chart_title){
 				}
 			}
 		}
-		//console.log(label_length_count);
-		//Pie chart of labels
+
 		var data = [{
 			values:[], 
-			labels:[], 
-			type: "pie", 
+			labels:[],
+			marker:{colors: [], line:{color:'rgb(100,100,100)', width:1}},
+			type: "pie",
 			textinfo: "value", 
 			name: ""
 		}];
 		var layout = {
-			grid: {rows: 2, columns: 1, pattern: 'independent'},
-			title: chart_title, 
+			// grid: {rows: 2, columns: 1, pattern: 'independent'},
+			// title: chart_title, 
 			showlegend: false,
-			autosize: false,
-			width: 600,
-			height: 1000,
+			// autosize: false,
+			// width: 600,
+			// height: 1000,
 			margin: {
-				l: 100,
-				r: 100,
-				b: 100,
+				l: 10,
+				r: 10,
+				b: 10,
 				t: 100,
-				pad: 1
 			},
-			yaxis2: {
-				title: "Publication count",
-			},
-			xaxis2: {
-				title: "Number of labels"
-			}
+			// yaxis2: {
+			// 	title: "Publication count",
+			// },
+			// xaxis2: {
+			// 	title: "Number of labels"
+			// }
 		}
 		//console.log(label_count);
 		for (var lab in label_count){
+			console.log(label_count[lab]);
 			data[0].values.push(label_count[lab]);
 			data[0].labels.push(lab);
+			data[0].marker.colors.push(facility_colour_map[lab]);
 		}
-		//doing bar chart for labels
-		data.push({
-			x:[], 
-			y:[], 
-			type: "bar",
-			hoverinfo: "value+label",
-			xaxis: 'x2', //this sets it to 
-			yaxis: 'y2',  //seconnd chart area
-			name: ""
-		});
-		for (var lab_len in label_length_count){
-			data[1].x.push(lab_len);
-			data[1].y.push(label_length_count[lab_len]);
-		}
+		// //doing bar chart for labels
+		// data.push({
+		// 	x:[], 
+		// 	y:[], 
+		// 	type: "bar",
+		// 	hoverinfo: "value+label",
+		// 	xaxis: 'x2', //this sets it to 
+		// 	yaxis: 'y2',  //seconnd chart area
+		// 	name: ""
+		// });
+		// for (var lab_len in label_length_count){
+		// 	data[1].x.push(lab_len);
+		// 	data[1].y.push(label_length_count[lab_len]);
+		// }
 
 		$(target_div).append('<div id="pie'+year+'" style="display:none;" class="piechart"></div>');
 		$('#button_holder').append('<button id="button'+year+'" class="year_button">'+year+'</button>');
