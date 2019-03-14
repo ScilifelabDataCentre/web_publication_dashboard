e7b9b8aa1812e241db58d83748c82d10c296fea1
Adrian <adr.lar@me.com>
2019-03-14 13:43:21 +0100

    loading logic overhaul
    Changed loading logic behaviour to have consistent tabs.

--- js/app.js
+++ js/app.js
@@ -72,7 +72,6 @@ function($, spin, wordcloud2, helpers, cytoscape_network, plotly_charts, current
 		$(this).toggleClass('active'); 
 	});
 
-
 	// Options for the loading animation
 	var opts = {
 		lines: 12,
@@ -89,132 +88,180 @@ function($, spin, wordcloud2, helpers, cytoscape_network, plotly_charts, current
 		speed: 1,
 		zIndex: 2e9,
 		className: 'spinner',
-		top: '50%',
+		top: '500px',
 		left: '50%',
 		shadow: '0 0 1px transparent',
 		position: 'absolute',
 	};
 
 	// Start the loading animations
-	var target_cyto = document.getElementById('spinner_cyto');
-	var spinner_cyto = new Spinner(opts).spin();
-	target_cyto.appendChild(spinner_cyto.el);
+	var target_current_status = document.getElementById('spinner_current_status');
+	var spinner_current_status = new Spinner(opts).spin();
+	target_current_status.appendChild(spinner_current_status.el);
 
-	var target_plotly = document.getElementById('spinner_plotly');
-	var spinner_plotly = new Spinner(opts).spin();
-	target_plotly.appendChild(spinner_plotly.el);
+	var target_facility_network = document.getElementById('spinner_facility_network');
+	var spinner_facility_network = new Spinner(opts).spin();
+	target_facility_network.appendChild(spinner_facility_network.el);
 
-	var target_current = document.getElementById('spinner_current');
-	var spinner_current = new Spinner(opts).spin();
-	target_current.appendChild(spinner_current.el);
+	var target_publication_stats = document.getElementById('spinner_publication_stats');
+	var spinner_publication_stats = new Spinner(opts).spin();
+	target_publication_stats.appendChild(spinner_publication_stats.el);
 
-	var target_stats = document.getElementById('spinner_stats');
-	var spinner_stats = new Spinner(opts).spin();
-	target_stats.appendChild(spinner_stats.el);
+	var target_facility_output = document.getElementById('spinner_facility_output');
+	var spinner_facility_output = new Spinner(opts).spin();
+	target_facility_output.appendChild(spinner_facility_output.el);
 
 	// Starts the masking of the word cloud canvas
 	mask_canvas();
 
-
 	// Start the rest of the processing
 	onReady(function () {
-		// API calls to publication.scilifelab.se
 
 		// Loaded flags
 		var loaded_bg = false;
-		var loaded_cyto = false;
-		var loaded_plotly = false;
-		var loaded_wordcloud = false;
-		var loaded_current = false;
-
-		var all_publications = null;
+		var loaded_current_status = false;
+		var loaded_facility_network = false;
+		var loaded_publication_stats = false;
+		var loaded_facility_output = false;
+		// LoadING flags
+		var loading_bg = false;
+		var loading_current_status = false;
+		var loading_facility_network = false;
+		var loading_publication_stats = false;
+		var loading_facility_output = false;
+		// Publication lists
+		var current_year_publications = null;
 		var recent_publications = null;
+		var all_publications = null;
 		var all_publications_pubmed_xml = null;
 
 		// Workers
 		// Starts to download everything in the background when page loads
-		let worker_background = new Worker('js/lib/fetch.js');
-
-		let worker_plotly = new Worker('js/lib/fetch.js');
-		let worker_cyto = new Worker('js/lib/fetch.js');
-		
-		// Fetches from publications db and pubmed
-		let worker_cloud = new Worker('js/lib/fetch_pubmed.js');
-		
-		// Worker return function for plotly
-		worker_plotly.onmessage = function(e) {
+		let worker_bg = new Worker('js/lib/fetch.js');
+		worker_bg.onmessage = function(e) {
 			all_publications = e.data;
+			loading_bg = false;
 			loaded_bg = true;
-			// console.log(worker_result);
-			// console.log('Message received from worker');
+		}
+		// Send message to worker_bg immediately
+		loading_bg = true;
+		worker_bg.postMessage(["https://publications.scilifelab.se/publications.json?full=false"]);
 
-			// Draw the pie charts
-			 draw_label_pie("#charts", all_publications, "Publication labels");
+		// Workers
+		// Starts to download this years data when page loads
+		let worker_current_status = new Worker('js/lib/fetch.js');
+		worker_current_status.onmessage = function(e) {
+			// Save publication list
+			current_year_publications = e.data;
+
+			// Draw table
+			conjure_table(current_year_publications, 2019);
 
 			// Turn off loading animation
-			show('spinner_plotly', false);
+			loading_current_status = false;
+			loaded_current_status = true;
+			show('spinner_current_status', false);
 		}
+		// Send message to worker_current_status immediately
+		loading_current_status = true;
+		worker_current_status.postMessage(["https://publications.scilifelab.se/publications/2019.json?full=false"]);
 
-		// Worker return function for cytoscape
-		worker_cyto.onmessage = function(e) {
+		// Gets recent publications and draws cytoscape network
+		let worker_facility_network = new Worker('js/lib/fetch.js');
+		worker_facility_network.onmessage = function(e) {
+			// Save publication list
 			recent_publications = e.data;
-			// Draw the cytoscape network	
-			draw_cyto("cytoscape_network", recent_publications);
-			
-			// Turn off loading animation
-			show('spinner_cyto', false);
-		}
 
-		// Worker return function for pubmed keywords
-		worker_cloud.onmessage = function(e) {
+			// Draw the cytoscape network
+			show('cytoscape_network', true);
+			draw_cyto("cytoscape_network", recent_publications);
+
+			// Turn off loading animation
+			loading_facility_network = false;
+			loaded_facility_network = true;
+			show('spinner_facility_network', false);
+		}		
+
+		// Fetches from publications db and pubmed
+		let worker_publication_stats = new Worker('js/lib/fetch_pubmed.js');
+		worker_publication_stats.onmessage = function(e) {
+			// This worker sends several messages with information on how much is loaded
+			// e.data[0] === true means it is done loading
 			if (e.data[0] === true){
+				// Save keyword xml
 				all_publications_pubmed_xml = e.data[1];
 
+				// Draw the wordcloud
 				draw_wordcloud(all_publications_pubmed_xml);
 
 				// Turn off loading animation
-				show('spinner_stats', false);				
+				loading_publication_stats = false;
+				loaded_publication_stats = true;
+				show('spinner_publication_stats', false);				
 			}
 			else {
+				// Not done loading yet, update the loading text
 				loading_level = e.data[1];
 				document.getElementById("loading_text_pubications_stats").innerHTML = "LOADING . . . <br/> "+loading_level+" %"
 			}
-
 		}
 
-		worker_background.onmessage = function(e) {
+		// Gets all publications and draws plotly charts for the facility output tab
+		let worker_facility_output = new Worker('js/lib/fetch.js');
+		worker_facility_output.onmessage = function(e) {
+			// Save publication list
 			all_publications = e.data;
-			loaded_bg = true;
-		}
-		worker_background.postMessage(["https://publications.scilifelab.se/publications.json?full=false"]);
 
-		$("#load_facility_output").click(function(){
+			// Draw the pie charts
+			draw_label_pie("#charts", all_publications, "Publication labels");
+
+			// Turn off loading animation
+			loading_facility_output = false;
+			loaded_facility_output = true;
+			show('spinner_facility_output', false);
+		}
+
+		$("#load_current_status").click(function(){
+			// Hide all dashes
 			$("#dashboards").children().hide();
-			$("#facility_output").show();
-			if (loaded_plotly === false) {
-				loaded_plotly = true;
-				show('spinner_plotly', true);
-				if (loaded_bg === true) {
-					// Draw the pie charts
-			 		draw_label_pie("#charts", all_publications);
-					show('spinner_plotly', false);
+			// Show this dashboard
+			$("#current_status").show();
+
+			if (loaded_current_status === false) {
+				// Show loading animation
+				show('spinner_current_status', true);
+				
+				if (loading_current_status === true) {
+					// Dont do anything at the moment, just waiting
 				}
 				else {
-					worker_plotly.postMessage(["https://publications.scilifelab.se/publications.json?full=false"]);
+					loading_current_status = true;
+					show('spinner_current_status', true);
+					worker_current_status.postMessage(["https://publications.scilifelab.se/publications/2019.json?full=false"]);
 				}
 			}
 		});
 		
 		$("#load_facility_network").click(function(){
+			// Hide all dashes
 			$("#dashboards").children().hide();
+			// Show this dashboard
 			$("#facility_network").show();
-			$("#cytoscape_network").show();
-			if (loaded_cyto === false){
-				loaded_cyto = true;
-				show('spinner_cyto', true);
-				worker_cyto.postMessage(["https://publications.scilifelab.se/publications/2017.json?full=false",
-					"https://publications.scilifelab.se/publications/2018.json?full=false",
-					"https://publications.scilifelab.se/publications/2019.json?full=false"]);
+
+			if (loaded_facility_network === false){
+				// Show loading animation
+				show('cytoscape_network', false);
+				show('spinner_facility_network', true);
+
+				if (loading_facility_network === true){
+					// Dont do anything at the moment, just waiting
+				}
+				else {
+					loading_facility_network = true;
+					worker_facility_network.postMessage(["https://publications.scilifelab.se/publications/2017.json?full=false",
+						"https://publications.scilifelab.se/publications/2018.json?full=false",
+						"https://publications.scilifelab.se/publications/2019.json?full=false"]);
+				}
 			}
 			else {
 				/*
@@ -225,46 +272,60 @@ function($, spin, wordcloud2, helpers, cytoscape_network, plotly_charts, current
 				draw_cyto("cytoscape_network", recent_publications);
 			}
 		});
-		$("#load_current_status").click(function(){
-			$("#dashboards").children().hide();
-			$("#current_status").show();
-			if (loaded_current === false) {
-				loaded_current = true;
-				show('spinner_current', true);
-				if (loaded_cyto === true){
-					conjure_table(recent_publications, 2019);
-				}
-				else if (loaded_bg === true){
-					conjure_table(all_publications, 2019);
-				}
-				else if (loaded_plotly === true){
-					conjure_table(all_publications, 2019);
-				}
-				else {
-					conjure_table(JSON.parse(Get("https://publications.scilifelab.se/publications/2019.json?full=false"))["publications"], 2019);
-				}
-				show('spinner_current', false);
-			}
-		});
+
 		$("#load_publication_stats").click(function(){
 			$("#dashboards").children().hide();
 			$("#publication_stats").show();
-			show('spinner_stats', true);
 
-			if (loaded_wordcloud === false) {
-				loaded_wordcloud = true;
-				if (loaded_bg === true){
-					worker_cloud.postMessage(all_publications);
+			if (loaded_publication_stats === false) {
+				// Show loading animation
+				show('spinner_publication_stats', true);
+
+				if (loading_publication_stats === true) {
+					// Dont do anything at the moment, just waiting
 				}
-				else {
-					worker_cloud.postMessage(["https://publications.scilifelab.se/publications.json?full=false"]);	
+				else{
+					loading_publication_stats = true;
+					if (loaded_bg === true){
+						// Just load the pubmed data if publications are already loaded
+						worker_publication_stats.postMessage(all_publications);
+					}
+					else {
+						// If bg is not loaded, load everything
+						worker_publication_stats.postMessage(["https://publications.scilifelab.se/publications.json?full=false"]);	
+					}
 				}
-			}
-			else{
-				// Turn off loading animation
-				show('spinner_stats', false);
 			}
 		});
+
+		$("#load_facility_output").click(function(){
+			$("#dashboards").children().hide();
+			$("#facility_output").show();
+
+			if (loaded_facility_output === false) {
+				// Show loading animation
+				show('spinner_facility_output', true);
+
+				if (loading_facility_output === true) {
+					// Dont do anything at the moment, just waiting
+				}
+				else {
+					loading_facility_output = true;
+					if (loaded_bg === true) {
+						// Draw the pie charts if publications are loaded already
+				 		draw_label_pie("#charts", all_publications);
+
+				 		// Turn off the loading animation
+				 		loaded_facility_output = true;
+						show('spinner_facility_output', false);
+					}
+					else {
+						worker_facility_output.postMessage(["https://publications.scilifelab.se/publications.json?full=false"]);
+					}
+				}
+			}
+		});
+
 		$("#load_current_status").click()
 	});
 });
